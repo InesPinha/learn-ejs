30ccde6c02376d15689fe5dedd5354952ed52767
Inês Piedade <inespinhamonteiro@gmail.com>
2022-12-09 09:45:07 +0000

    new changes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require ("express"); 
+const app= express(); 
+
+app.set("view engine", "ejs"); 
+
+app.get("/", function(req,res){
+    const items =[
+        {
+            title:"D",
+            message:"esenvolver aplicações/ serviços de forma facil"
+        },
+        {
+            title:"E",
+            message:"JS usa javascript para renderizar HTML"
+        },
+        {
+            title:"M",
+            message:"uito fácil de usar"
+        },
+        {
+            title:"A",
+            message:"prender é sempre bom"
+        },        
+        {
+            title:"I",
+            message:"nstall ejs"
+        },
+        {
+            title:"S",
+            message:"intaxe simples"
+        }
+    ]
+    const subtitle = "Subtitulo vindo do servidor"
+    res.render("pages/index", {qualitys:items, subtitle})
+})
+
+app.get("/sobre", function(req,res){
+    res.render("pages/about")
+})
+
+app.listen(8080); 
+console.log('oi')
